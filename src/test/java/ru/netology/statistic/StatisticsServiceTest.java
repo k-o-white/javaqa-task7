0d75857953a43cdaafe0afc6b034cdc09205ec01
Kirill Burilichev <koburilichev@gmail.com>
2026-02-10 12:31:01 +0300

    StatisticsService class and tests created

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class StatisticsServiceTest {
+
+    @Test
+    public void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxInTheMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {3, 5, 8, 4, 5, 12, 8, 6, 11, 11, 10};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
